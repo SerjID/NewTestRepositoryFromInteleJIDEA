ddee25ebf04b6e4d481b40f668a967094ad49b13
SerjID <serj.c4@gmail.com>
2022-10-11 15:26:46 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello " + MyClass.name[3] + " with ID = " + MyClass.id[3]);
+    }
+}
---
+++ src/MyClass.java
@@ -0,0 +1,4 @@
+public class MyClass {
+    static Integer[] id = {1,2,3,4};
+    static String[] name = {"Serj","Ihor","Alan","Fernando"};
+}
